1c9c53854bc1d9803da649acc964b30a58c571fa
Noubar Topalian <topalian.noubar@gmail.com>
2020-03-07 12:01:59 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 //Code goes here
+window.onload=()=>{
+    document.getElementById("start-button").onclick=()=>{
+        startGame()
+    }
+}
+
+function startGame(){
+    //draw()
+}
+//WHATEVER!!!!!
